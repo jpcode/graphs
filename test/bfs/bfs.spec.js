8142e55b83937c00c24f66955afa42b22e506cda
Cardenas Alvarado, John P <john.cardenas@one.verizon.com>
2017-04-11 18:08:57 -0400

    adding dfs/bfs tests

--- test/bfs/bfs.spec.js
+++ test/bfs/bfs.spec.js
@@ -5,7 +5,7 @@ var bfs = require('../../src/bfs/bfs').bfs;
 
 describe('BFS', function () {
 
-  it('Init graph and traverse', function () {
+  it('BFS should traversal graph 0-1-2-3-4', function () {
     var g = new Graph( 5 );
     g.addEdge(0,1);
     g.addEdge(0,2);
@@ -15,5 +15,27 @@ describe('BFS', function () {
     bfs(g, 0);
   });
 
+  it('BFS should traversal graph: 0-1-3-2', function() {
+        var g = new Graph(4);
+        g.addEdge(0,1);
+        g.addEdge(0,3);
+        g.addEdge(1,2);
+        g.addEdge(2,3);
+        g.showGraph();
+        bfs( g, 0 );
+        //assert.equal(12, sum( 8, 4 ) );
+    });
+
+  it('BFS should traversal graph: 0-1-2-3', function() {
+        var g = new Graph(4);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,2);
+        g.addEdge(1,3);
+        g.addEdge(3,2);
+        g.showGraph();
+        bfs( g, 0 );
+        //assert.equal(12, sum( 8, 4 ) );
+    });
 
 });
--- test/dfs/dfs.spec.js
+++ test/dfs/dfs.spec.js
@@ -4,12 +4,34 @@ var Graph = require('../../src/graph').Graph;
 var dfs = require('../../src/dfs/dfs').dfs;
 describe('dfs', function() {
   describe('dfs()', function() {
-    it('should traversal graph: 0-1-3-2-4', function() {
+    it('DFS should traversal graph: 0-1-3-2-4', function() {
         var g = new Graph(5);
         g.addEdge(0,1);
         g.addEdge(0,2);
         g.addEdge(1,3);
         g.addEdge(2,4);
+        g.showGraph();
+        dfs( g, 0 );
+        //assert.equal(12, sum( 8, 4 ) );
+    });
+     it('DFS should traversal graph: 0-1-2-3', function() {
+        var g = new Graph(4);
+        g.addEdge(0,1);
+        g.addEdge(0,3);
+        g.addEdge(1,2);
+        g.addEdge(2,3);
+        g.showGraph();
+        dfs( g, 0 );
+        //assert.equal(12, sum( 8, 4 ) );
+    });
+
+      it('DFS should traversal graph: 0-1-3-2', function() {
+        var g = new Graph(4);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,2);
+        g.addEdge(1,3);
+        g.addEdge(3,2);
         g.showGraph();
         dfs( g, 0 );
         //assert.equal(12, sum( 8, 4 ) );
