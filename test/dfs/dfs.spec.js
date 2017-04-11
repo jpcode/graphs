8e5181f47867907d4df1657beb01ef73a787a066
John Cardenas <john.cardenas@cpe.pe>
2017-04-11 17:38:36 -0500

    fix test

--- test/dfs/dfs.spec.js
+++ test/dfs/dfs.spec.js
@@ -29,7 +29,6 @@ describe('dfs', function() {
         var g = new Graph(4);
         g.addEdge(0,1);
         g.addEdge(0,2);
-        g.addEdge(1,2);
         g.addEdge(1,3);
         g.addEdge(3,2);
         g.showGraph();
